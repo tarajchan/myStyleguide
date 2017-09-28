72a7359cbcdcaac170dc16cc048329150fe2cfc8
Tara Chan <tarajchan98@gmail.com>
2017-09-28 01:48:38 -0700

    first commit in a while

--- myStyleGuide.js
+++ myStyleGuide.js
@@ -4,19 +4,70 @@ document.addEventListener("DOMContentLoaded", main);
 
 function main() {
 	"use strict";
-	let navDropDown = document.querySelector(".navigationBar__dropDown");
+	let dropDownArrow = document.querySelectorAll(".triangle--down-white");
+	let buttonAbs = document.querySelectorAll(".button--absolute");
+	let accordion = document.querySelectorAll(".accordion");
 
-	navDropDown.addEventListener("click", showDropDown(), false);
+	for (var i = 0; i < buttonAbs.length; i++) {
+		buttonAbs[i].addEventListener("click", toggle(i), false);
+	}
+
+	for (var i = 0; i < accordion.length; i++) {
+		accordion[i].addEventListener("click", toggle(i), false);
+	}
+
+	for (var i = 0; i < dropDownArrow.length; i++) {
+		dropDownArrow[i].addEventListener("click", toggleDropDown(i), false);
+	}
+
+	if (matchMedia) {
+		const MQ_TABLETBP = window.matchMedia("@media screen and (min-width: 640px)");
+		MQ_TABLETBP.addListener(WidthChange);
+		WidthChange(MQ_TABLETBP);
+	}
+} 
+
+function toggle(i) {
+	return function() {
+		this.classList.toggle("active");
+		this.nextElementSibling.classList.toggle("show");
+	};
 }
 
-function showDropDown(event) {
-	let action = event.currentTarget;
-	let dropDownMenu = document.querySelector(".navigationBar__dropDown-menu");
-	let hidden = document.querySelectorAll(".hidden");
+function toggleDropDown(i) {
+	return function() {
+		this.parentNode.classList.toggle("active");
+		this.nextElementSibling.classList.toggle("show");
+	};
+}
 
+function WidthChange(MQ_TABLETBP) {
+	let mainWrapper = document.querySelectorAll(".main__sectionDescription-wrapper");
+
+	if (MQ_TABLETBP.matches) {
+		mainWrapper.classList.remove("grid__container--2");
+		mainWrapper.classList.add("grid__container--3");
+	}
+}
+
+/*function toggleDropDown(i) {
+	let sideModules = document.querySelectorAll(".sidebar__module"); 
+	let parentNode = this.parentNode.className;
+
+	return function(event) {
+		let clickedElement = event.currentTarget;
+
+		for (var i = 0; i < sideModules.length; i++) {
+			sideModules[i].className = "sidebar__module closed";
+			if (parentNode.classList.contains("closed")) {
+					parentNode.classList.toggle("closed");
+			}
+		}
+	};
+}
+*/
 	
-		dropDownMenu.classList.toggle(hidden);
-}
+		
 
 
 
